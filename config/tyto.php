9960a9c9cc9460ca5158d6cd7dcc7ae2d794cc07
Josue Gomes <josuerodrigovieira@gmail.com>
2026-08-23 12:06:19 -0300

    refactor: harden Tyto ingestion core

--- config/tyto.php
+++ config/tyto.php
@@ -14,4 +14,11 @@ return [
         'composer' => env('TYTO_COMPOSER_BINARY', env('LARAOWL_COMPOSER_BINARY', 'composer')),
         'npm' => env('TYTO_NPM_BINARY', env('LARAOWL_NPM_BINARY', 'npm')),
     ],
+
+    'ingestion' => [
+        'max_payload_bytes' => (int) env('TYTO_INGEST_MAX_PAYLOAD_BYTES', 1048576),
+        'max_records_per_batch' => (int) env('TYTO_INGEST_MAX_RECORDS', 500),
+        'rate_limit_per_minute' => (int) env('TYTO_INGEST_RATE_LIMIT', 600),
+        'idempotency_ttl' => (int) env('TYTO_INGEST_IDEMPOTENCY_TTL', 86400),
+    ],
 ];
