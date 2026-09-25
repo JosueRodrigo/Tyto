62d79c382a21b76acebe8f964c2e547f592f6ce9
Josue Gomes <josuerodrigovieira@gmail.com>
2026-09-25 00:04:04 -0300

    fix: persist issue resolution timestamp

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -21,11 +21,13 @@ class Issue extends Model
         'users_count',
         'first_seen_at',
         'last_seen_at',
+        'resolved_at',
     ];
 
     protected $casts = [
         'first_seen_at' => 'datetime',
         'last_seen_at' => 'datetime',
+        'resolved_at' => 'datetime',
     ];
 
     /**
